22227275cd583eeca637484b45d77b5662198217
Your Name <you@example.com>
2022-06-23 13:13:39 +0000

    support nix

--- flake.nix
+++ flake.nix
@@ -1,45 +1,26 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs";
+    nixpkgs.url = "nixpkgs"; 
     utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, utils, naersk, ... }:
+  outputs = { self,utils ,nixpkgs, ... }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        naersk-lib = naersk.lib."${system}";
       in
       rec {
-        # `nix build`
-        packages.cargo-bpf = naersk-lib.buildPackage {
-          meta = with pkgs.lib; {
-            description = "Rust eBPF tooling";
-            homepage = "https://foniod.org";
-            license = licenses.mit;
-            platforms = [ "x86_64-linux" "aarch64-linux" ];
-          };
-
-          name = "cargo-bpf";
-          version = "3.0.0";
-
-          src = ./.;
-          root = ./.;
-          gitSubmodules = true;
-
-          LIBCLANG_PATH = "${pkgs.llvmPackages_13.libclang.lib}/lib";
-          KERNEL_SOURCE = "${pkgs.linuxPackages.kernel.dev}/lib/modules/${pkgs.linuxPackages.kernel.version}";
-
+        devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             pkgconfig
             llvm_13
             clang_13
-          ];
+            rustup
+            rust-analyzer
+            cargo-watch
+            glibc
+            git
+           ];
           buildInputs = with pkgs; [
             openssl
             zlib
@@ -47,24 +28,7 @@
             libelf
             llvm_13.dev
             clang_13
-            linuxPackages.kernel.dev
-            linuxHeaders
-            glibc.dev
-          ];
-        };
-        defaultPackage = packages.cargo-bpf;
-
-        defaultApp = apps.cargo-bpf;
-        apps.cargo-bpf = {
-          type = "app";
-          program = "${self.defaultPackage."${system}"}/bin/cargo-bpf";
-        };
-
-        # `nix develop`
-        devShell = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.cargo-bpf ];
-          nativeBuildInputs = with pkgs; [
-            rustup
+            glibc
           ];
 
           LIBCLANG_PATH = "${pkgs.llvmPackages_13.libclang.lib}/lib";
